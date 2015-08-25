67161d18bf0d2427111c7ebb512272c53e2ed211
martincolts <jmartinlopez89@gmail.com>
2015-08-25 18:49:08 -0300

    Issue x - Arreglando la clase persona (Agredado el creador)

--- Persona.java
+++ Persona.java
@@ -1,6 +1,10 @@
-public class persona {
+public class Persona {
   private String nombre ;
   priavet String apellido ;
-
+  
+  public Persona (String nombre , String apellido ){
+	  this.nombre = nombre ;
+	  this.apellido = apellido ;
+  }
 
 }
